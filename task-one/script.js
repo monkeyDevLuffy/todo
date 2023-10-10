d59e1c697f7371bbb6eeee35d12c3d885d43644b
Abhishek <abhishek.premarajan@qburst.com>
2023-10-10 10:02:20 +0530

    embedded a video as modal

--- task-one/script.js
+++ task-one/script.js
@@ -1,5 +1,7 @@
 const openModalButtons=document.querySelectorAll('[data-modal-target]');
 const closeModalButtons=document.querySelectorAll('[data-modal-close]');
+const openMusicModalButtons=document.querySelectorAll('[data-music-modal-target]');
+const closeMusicModalButtons=document.querySelectorAll('[data-music-modal-close]');
 const overlay=document.getElementById('overlay');
 
 openModalButtons.forEach(button=>{
@@ -14,6 +16,20 @@ closeModalButtons.forEach(button=>{
         closeModal(modal);
     })
 })
+
+openMusicModalButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        const music_modal = document.getElementById('music-modal');
+        openModal(music_modal);
+    })
+})
+closeMusicModalButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        const music_modal = document.getElementById('music-modal');
+        closeModal(music_modal);
+    })
+})
+
 function openModal(modal){
     if(modal==null) return null;
     modal.classList.add('active');
