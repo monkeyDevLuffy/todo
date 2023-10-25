b2653ee8e16f431694eb27bf772317d718420047
Abhishek <abhishek.premarajan@qburst.com>
2023-10-25 14:30:25 +0530

    added delete functionality to projects

--- todo-app/scripts/script.js
+++ todo-app/scripts/script.js
@@ -2,11 +2,27 @@
 function createNewProject(){
     const project = document.createElement("div");
     const name = document.createTextNode(`project ${projects.childElementCount+1}`);
+    const deletebutton = document.createElement('span');
+    deletebutton.className="material-symbols-outlined deleteButton";
+    deletebutton.innerText="delete";
     project.className="card";
     project.setAttribute("data-id",`${projects.childElementCount+1}`)
     project.appendChild(name);
+    project.appendChild(deletebutton)
     projects.appendChild(project);
+
+
 }
+function deleteProject(event){
+    if(event.target.classList.contains("deleteButton")){
+        console.log(true);
+        console.log(event.target.parentElement);
+        projects.removeChild(event.target.parentElement);
+    }else{
+        console.log(false);
+    }
+}
+
 
 
 
@@ -16,9 +32,11 @@ console.log(createProjectButton);
 
 
 
-
 //event listeners
 
 createProjectButton.addEventListener("click",()=>{
-    createNewProject();    
+    createNewProject();  
+})
+projects.addEventListener("click",(event)=>{
+    deleteProject(event);
 })
