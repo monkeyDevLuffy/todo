b9f77e0545b06c646ff5f7b522f6e4cd5d0ab283
Abhishek <abhishek.premarajan@qburst.com>
2023-10-27 14:38:58 +0530

    added functionality to change task status to completed

--- todo-app/users/abhishek/tasks.js
+++ todo-app/users/abhishek/tasks.js
@@ -14,21 +14,34 @@ function addNewTask(event){
         todoOl.appendChild(newTask);
     }
 
+
 }
+function changeTaskStatus(event){
+    if(event.target.type==="checkbox"){
+        const task = event.target.parentElement;
+        if(task.parentElement.id=="todo-ol"){
+            if(event.target.checked){
+                todoOl.removeChild(task);
+                completedOl.appendChild(task);
+            }
+        }else if(task.parentElement.id=="completed-ol"){
+            if(!event.target.checked){
+                completedOl.removeChild(task);
+                todoOl.appendChild(task); 
+            }
+        }
+        
+    }
+    //add delete fuctionality here
+}
+
 const taskPopup=document.getElementById("add-task-dialog");
 
 const openTaskDialogButton = document.querySelector("#open-add-task");
 const addTaskButton = document.querySelector("#add-task-button");
 const dialogInput = document.querySelector("dialog input")
-const todoOl=document.getElementById("todo-ordered-list");
-console.log(todoOl);
-
-
-
-
-
-
-
+const todoOl=document.getElementById("todo-ol");
+const completedOl = document.getElementById("completed-ol");
 
 
 
@@ -38,4 +51,5 @@ console.log(todoOl);
 //event listeners
 openTaskDialogButton.addEventListener("click",showTaskPopup);
 addTaskButton.addEventListener("click",addNewTask);
-
+todoOl.addEventListener("click",changeTaskStatus);
+completedOl.addEventListener("click",changeTaskStatus);
